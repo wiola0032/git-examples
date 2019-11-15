e501997d2048dc6a434aa59e47ddcf7114d914eb
wiola <wiolac0032@wp.pl>
2019-11-15 14:50:45 +0100

    [change] - hello my friend

--- src/main/java/pl/mpas/git_examples/Hello.java
+++ src/main/java/pl/mpas/git_examples/Hello.java
@@ -4,5 +4,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello git");
         System.out.println("Hello Kamil");
+        System.out.println("Hello Kamil again");
     }
 }
