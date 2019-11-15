18522b515ad9c5960d24bd89831caa27b794f01e
wiola <wiolac0032@wp.pl>
2019-11-15 11:17:04 +0100

    another line in main function

--- src/main/java/pl/mpas/git_examples/Hello.java
+++ src/main/java/pl/mpas/git_examples/Hello.java
@@ -3,5 +3,6 @@ package pl.mpas.git_examples;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello git");
+        System.out.println("Hello Kamil");
     }
 }
