7e87e755434afeab5cfe0fad294f126c5538c5bf
wiola <wiolac0032@wp.pl>
2019-11-15 11:41:58 +0100

    add constructor

--- src/main/java/pl/mpas/git_examples/car/model/Car.java
+++ src/main/java/pl/mpas/git_examples/car/model/Car.java
@@ -1,10 +1,12 @@
 package pl.mpas.git_examples.car.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.math.BigDecimal;
 
 @Data
+@AllArgsConstructor
 public class Car {
 
     private String brand;
