f10537e5ff0321bfb8a3977ccd1029bb2ed789e1
wiola <wiolac0032@wp.pl>
2019-11-15 15:51:12 +0100

    [change] - fixing message to friend v2

--- src/main/java/pl/mpas/git_examples/Hello.java
+++ src/main/java/pl/mpas/git_examples/Hello.java
@@ -2,9 +2,9 @@ package pl.mpas.git_examples;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello git");
+
         System.out.println("Hello Kamil");
-        System.out.println("Hello Kamil again!!!");
+
         System.out.println("hasta la vista");
     }
 }
