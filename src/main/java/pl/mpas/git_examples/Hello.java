717cff2d6c74605dbca91ab3904fe3392d0e63ef
wiola <wiolac0032@wp.pl>
2019-11-15 15:52:33 +0100

    [add] - fixing message

--- src/main/java/pl/mpas/git_examples/Hello.java
+++ src/main/java/pl/mpas/git_examples/Hello.java
@@ -4,7 +4,7 @@ public class Hello {
     public static void main(String[] args) {
 
         System.out.println("Hello Kamil");
-
+        System.out.println("Kaml pupilek Kamila <3");
         System.out.println("hasta la vista");
     }
 }
